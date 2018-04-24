dfff556ba2c89d5444a8dfec1a7ce97564b3660f
Ray Zhou <zhou.ray@gmail.com>
2018-04-23 23:30:18 -0700

    material for simple redux tutorial

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store/index";
+import App from "./components/App";
+import { addArticle } from "../js/actions/index";
+
+window.store = store;
+window.addArticle = addArticle;
+
+render (
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
